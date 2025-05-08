eb24fdc794bbd8c77bebb4dd8abd5c2873fc6eb8
imvg93 <gireeshv03@gmail.com>
2025-05-08 23:42:17 +0530

    Update duration for Character Makeup service to reflect accurate time estimate.

--- pages/services.js
+++ pages/services.js
@@ -372,7 +372,7 @@ const Services = () => {
         name: 'Character Makeup',
         description: 'Creative character and theme-based makeup',
         image: '/images/services/character Makeup.jpg',
-        duration: '1-2 hours'
+        duration: '1 hours'
       },
       {
         name: 'Airbrush Makeup',
